46263291e4906ab4bd55840696a4f0e43ccc592e
srxshiv <srxshiv@gmail.com>
2025-06-10 00:53:33 +0530

    Vapi workflow updates

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -9,7 +9,7 @@ export async function GET(){
 
 export async function POST(request: Request){
     const { type , role , level, techstack , amount , userid } = await request.json();
-
+    console.log("Interview generation request received")
     try{
         const {text : questions} = await generateText({
             model: google("gemini-2.0-flash-lite"),
--- components/Agent.tsx
+++ components/Agent.tsx
@@ -71,7 +71,7 @@ const Agent = ({ userName, userId, type }: AgentProps) => {
     setCallStatus(CallStatus.CONNECTING);
     try {
       if (type === "generate") {
-        await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!, {
+        await vapi.start(undefined , undefined , undefined , process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!,{
           variableValues: {
             username: userName,
             userid: userId,
